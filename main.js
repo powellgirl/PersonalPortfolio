5de064a94b7f4a9222f84388f18c48a8ce4ffd33
Lauren Tolley <powellgirl360@gmail.com>
2020-07-22 17:44:00 -0700

    set up structure

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ function append (parent, el) {
     return parent.appendChild(el);
 }
 
+const names = document.getElementById('names');
+
+let li = createNode('li'),
+img = createNode('img'),
+span = createNode('span'),
+
+img.src= people.name;
+span.innerHTML = `${people.name} ${people.homeworld}`
+append(li, img);
+append(li, span);
+append(ul, li);
